308826a60b7b7981f8687c5c2123091b376bb110
CharlesYWL <xlyin@ucdavis.edu>
2018-01-16 23:36:55 -0800

    IMPL predict

--- handwritten_digits.py
+++ handwritten_digits.py
@@ -21,6 +21,8 @@ def csv_to_nparray(path):
 
 X_total = csv_to_nparray('xTotal.csv')
 y_total = csv_to_nparray('yTotal.csv')
+for i in range(0, y_total.shape[0]):
+    y_total[i] = y_total[i] if y_total[i] != 10 else 0 # label 10 -> label 0
 X_total, y_total = shuffle(X_total, y_total)
 X_train = X_total[:3000];
 X_cv = X_total[3000:4000];
@@ -29,38 +31,38 @@ y_train = y_total[:3000];
 y_cv = y_total[3000:4000];
 y_test = y_total[4000:];
 
-
-for val in y_train:
-    print(np.array_equal(val, [1]))
-
-exit(0)
-
-mlps = [];
-for label in range(1, 11):
-    mlps[label if label < 10 else 0] = MLPClassifier(hidden_layer_sizes=(25,), max_iter = 400, alpha = 1e-4,
-                                                        solver = 'sgd', verbose = 10, tol = 1e-4, random_state = 1,
-                                                        learning_rate_init = .1)
+mlps = [] # mlps[i]: training model for label i
+for label in range(0, 10):
     y_train_label = []
     for val in y_train:
-        y_train_label.append([y_train[1]])
+        y_train_label.append(1 if val == label else 0)
+    mlps.append(MLPClassifier(hidden_layer_sizes=(25,), max_iter = 400, alpha = 1e-4,
+                                solver = 'sgd', verbose = 10, tol = 1e-4, random_state = 1,
+                                learning_rate_init = .1))
+    mlps[-1].fit(X_train, y_train_label)
 
+def predict(mlps, X_test):
+    '''\
+        returns prediction matrix, matrix[i]: predict probability from num_classes mlps
+        matrix has shape (num_sample, num_classes)
+    '''
+    if X_test.ndim == 1: # edge case for num_test == 1
+        X_test = np.array([X_test], np.dtype(float))
 
+    probability = [] # has len num_classes
+    for mlp in mlps:
+        probability.append(mlp.predict_proba(X_test)[:, 1])
+    return np.array(probability).T
 
-mlp = MLPClassifier(hidden_layer_sizes=(25,), max_iter = 400, alpha = 1e-4,
-                    solver = 'sgd', verbose = 10, tol = 1e-4, random_state = 1,
-                    learning_rate_init = .1)
-mlp.fit(X_train, y_train)
+probability = predict(mlps, X_test) # size(num_test, num_classes)
+predictions = np.argmax(probability, axis = 1)
+y_test = np.array(y_test, np.dtype(int)).flatten()
+# print(probability)
+# print(predictions)
+# print(y_test)
 
-print("Training set score: %f" % mlp.score(X_train, y_train))
-print("Test set score: %f" % mlp.score(X_test, y_test))
+num_correct = 0
+for i in range(0, X_test.shape[0]):
+    num_correct += 1 if predictions[i] == y_test[i] else 0
 
-# fig, axes = plt.subplots(4, 4)
-# # use global min / max to ensure all weights are shown on the same scale
-# vmin, vmax = mlp.coefs_[0].min(), mlp.coefs_[0].max()
-# for coef, ax in zip(mlp.coefs_[0].T, axes.ravel()):
-#     ax.matshow(coef.reshape(28, 28), cmap=plt.cm.gray, vmin=.5 * vmin,
-#                vmax=.5 * vmax)
-#     ax.set_xticks(())
-#     ax.set_yticks(())
-#
-# plt.show()
+print(num_correct / X_test.shape[0])
