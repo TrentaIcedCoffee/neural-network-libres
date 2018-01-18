155f4b0bd04a889b14bf960180bd212003497cc0
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-18 14:41:08 -0800

    wrapped

--- handwritten_digits.py
+++ handwritten_digits.py
@@ -13,17 +13,6 @@ import numpy as np
 import datah
 import time
 
-# data preperation
-X_total = datah.Data('xTotal.csv').to_nparray(float)
-y_total = datah.Data('yTotal.csv').to_nparray(int)
-# label 10 -> label 0
-for i in range(0, y_total.shape[0]):
-    y_total[i] = y_total[i] if y_total[i] != 10 else 0
-X_total, y_total = shuffle(X_total, y_total)
-X_train, y_train = X_total[:3000], y_total[:3000]
-X_cv, y_cv = X_total[3000:4000], y_total[3000:4000]
-X_test, y_test = X_total[4000:], y_total[4000:]
-
 def regulate(x):
 	'''\
 		add one extra dim when x.ndim == 1
@@ -108,20 +97,41 @@ def optimize_regulating_rate(X_train, y_train, X_cv, y_cv):
 			regulating_rate_opt = regulating_rate
 	return regulating_rate_opt
 
-start = time.time()
+def prepare_data(x_path, y_path):
+	X_total = datah.Data(x_path).to_nparray(float)
+	y_total = datah.Data(y_path).to_nparray(int)
+	# label 10 -> label 0
+	for i in range(0, y_total.shape[0]):
+	    y_total[i] = y_total[i] if y_total[i] != 10 else 0
+	X_total, y_total = shuffle(X_total, y_total)
+	X_train, y_train = X_total[:3000], y_total[:3000]
+	X_cv, y_cv = X_total[3000:4000], y_total[3000:4000]
+	X_test, y_test = X_total[4000:], y_total[4000:]
+	return (X_train, y_train, X_cv, y_cv, X_test, y_test)
 
-num_sample_opt = optimize_num_sample(X_train, y_train, X_cv, y_cv)
-X_train = X_train[:num_sample_opt]
-y_train = y_train[:num_sample_opt]
+def main():
+	X_train, y_train, X_cv, y_cv, X_test, y_test = prepare_data('xTotal.csv', 'yTotal.csv')
+	# default, no optimization
+	num_sample_opt = X_train.shape[0];
+	regulating_rate_opt = 0;
+	start = time.time()
 
-regulating_rate_opt = optimize_regulating_rate(X_train, y_train, X_cv, y_cv)
+	# run on a production machine
+	# num_sample_opt = optimize_num_sample(X_train, y_train, X_cv, y_cv)
+	# X_train = X_train[:num_sample_opt]
+	# y_train = y_train[:num_sample_opt]
+	#
+	# regulating_rate_opt = optimize_regulating_rate(X_train, y_train, X_cv, y_cv)
 
-mlps = train(X_train, y_train, regulating_rate_opt)
-hypo = predict(mlps, X_test)
-precision = judge(hypo, y_test)
+	mlps = train(X_train, y_train, regulating_rate_opt)
+	hypo = predict(mlps, X_test)
+	precision = judge(hypo, y_test)
 
-print(num_sample_opt)
-print(regulating_rate_opt)
-print(precision)
+	end = time.time()
 
-print(time.time() - start)
+	print('num_sample_opt (sample number): {}'.format(num_sample_opt))
+	print('regulating_rate_opt (lambda): {}'.format(regulating_rate_opt))
+	print('precision: {}'.format(precision))
+	print('time elapsed: {}'.format(end - start))
+
+if __name__ == '__main__': main()
---
+++ run.cmd
@@ -0,0 +1,1 @@
+python handwritten_digits.py
