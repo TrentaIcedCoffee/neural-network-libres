786d6af9be215a3f28d2dbb95628bca1c0ce0af0
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-17 18:45:18 -0800

    finished

--- handwritten_digits.py
+++ handwritten_digits.py
@@ -4,11 +4,14 @@
 '''
 
 import matplotlib.pyplot as plt
+
 from sklearn.utils import shuffle
 from sklearn.datasets import fetch_mldata
 from sklearn.neural_network import MLPClassifier
 import numpy as np
+
 import datah
+import time
 
 # data preperation
 X_total = datah.Data('xTotal.csv').to_nparray(float)
@@ -20,6 +23,14 @@ X_total, y_total = shuffle(X_total, y_total)
 X_train, y_train = X_total[:3000], y_total[:3000]
 X_cv, y_cv = X_total[3000:4000], y_total[3000:4000]
 X_test, y_test = X_total[4000:], y_total[4000:]
+
+def regulate(x):
+	'''\
+		add one extra dim when x.ndim == 1
+	'''
+	if x.ndim == 1:
+		x = np.array([x], x.dtype)
+	return x
 
 def train(X_train, y_train, regulating_rate):
     '''\
@@ -72,7 +83,45 @@ def judge(hypo, y):
 	    num_correct += 1 if hypo[i] == y[i] else 0
 	return num_correct / y.shape[0]
 
-mlps = train(X_train, y_train, 1e-4)
+def optimize_num_sample(X_train, y_train, X_cv, y_cv):
+	num_sample_opt = 0
+	precision_opt = 0
+	for num_sample in range(1, 10):
+		mlps = train(X_train[:num_sample], y_train[:num_sample], 0)
+		hypo = predict(mlps, X_cv)
+		precision = judge(hypo, y_cv)
+		if precision > precision_opt:
+			precision_opt = precision
+			num_sample_opt = num_sample
+	return num_sample_opt
+
+def optimize_regulating_rate(X_train, y_train, X_cv, y_cv):
+	regulating_rate_opt = 0
+	precision_opt = 0
+	for i in range(0, 1000):
+		regulating_rate = i / 100 # regulating_rate: [0, 10) with precision 0.01
+		mlps = train(X_train, y_train, regulating_rate)
+		hypo = predict(mlps, X_cv)
+		precision = judge(hypo, y_cv)
+		if precision > precision_opt:
+			precision_opt = precision
+			regulating_rate_opt = regulating_rate
+	return regulating_rate_opt
+
+start = time.time()
+
+num_sample_opt = optimize_num_sample(X_train, y_train, X_cv, y_cv)
+X_train = X_train[:num_sample_opt]
+y_train = y_train[:num_sample_opt]
+
+regulating_rate_opt = optimize_regulating_rate(X_train, y_train, X_cv, y_cv)
+
+mlps = train(X_train, y_train, regulating_rate_opt)
 hypo = predict(mlps, X_test)
 precision = judge(hypo, y_test)
+
+print(num_sample_opt)
+print(regulating_rate_opt)
 print(precision)
+
+print(time.time() - start)
