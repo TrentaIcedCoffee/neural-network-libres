a9834cd9e68778382d13f5fd509ac73d28aac8f8
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-21 15:48:53 -0800

    FIXED judge -> **kwargs

--- nnp.py
+++ nnp.py
@@ -16,38 +16,38 @@ def regulate(X: 'nparray') -> 'nparray':
 
 def main():
     # data from csv
-    X_path = './data_handwritten_digits/X_total.csv'
-    y_path = './data_handwritten_digits/y_total.csv'
-    X_total, y_total = datah.Data(X_path).to_nparray(float), datah.Data(y_path).to_nparray(int)
-    X_total, y_total = regulate(X_total), regulate(y_total)
+    # X_path = './data_handwritten_digits/X_total.csv'
+    # X_total, y_total = datah.Data(X_path).to_nparray(float), datah.Data(y_path).to_nparray(int)
+    # y_path = './data_handwritten_digits/y_total.csv'
+    # X_total, y_total = regulate(X_total), regulate(y_total)
     # y 10 -> 0, in mat file 10 -> 0
-    for i in range(0, y_total.shape[0]):
-        y_total[i] = y_total[i] if y_total[i] != 10 else 0
+    # for i in range(0, y_total.shape[0]):
+    #     y_total[i] = y_total[i] if y_total[i] != 10 else 0
     # shuffle
-    X_total, y_total = shuffle(X_total, y_total)
+    # X_total, y_total = shuffle(X_total, y_total)
     # split
-    X_train, y_train = X_total[:3000], y_total[:3000]
-    X_cv, y_cv = X_total[3000:4000], y_total[3000:4000]
+    # X_train, y_train = X_total[:3000], y_total[:3000]
+    # X_cv, y_cv = X_total[3000:4000], y_total[3000:4000]
     # X_test, y_test = X_total[4000:], y_total[4000:]
 
     # prepared data
-    # box = stor.Box('data_debug')
+    box = stor.Box('data_debug')
     # X_total, y_total = box.get('X_total'), box.get('y_total')
-    # X_train, y_train = box.get('X_train'), box.get('y_train')
-    # X_cv, y_cv = box.get('X_cv'), box.get('y_cv')
-    # X_test, y_test = box.get('X_test'), box.get('y_test')
+    X_train, y_train = box.get('X_train'), box.get('y_train')
+    X_cv, y_cv = box.get('X_cv'), box.get('y_cv')
+    X_test, y_test = box.get('X_test'), box.get('y_test')
 
     # train
-    mlps = train(X_train, y_train)
+    # mlps = train(X_train, y_train)
 
     # trained mlps
-    # mlps = box.get('mlps')
+    mlps = box.get('mlps')
 
     # predict y_hypo
-    y_cv_hypo = predict(mlps, X_cv)
+    y_test_hypo = predict(mlps, X_test)
 
     # judge
-    precision = judge(y_cv, y_cv_hypo)
+    precision = judge(y = y_test, y_hypo = y_test_hypo)
     print(precision)
 
 # def opt_num_sample(X_train, y_train, X_cv, y_cv, p_range = None, regulating_rate = 0):
@@ -57,7 +57,8 @@ def main():
 #     precision_opt = 0
 #     for num_sample in range(p_range(0), p_range(1) + 1):
 #         mlps = train(X_train[:num_sample], y_train[:num_sample], regulating_rate)
-#         hypo = predict()
+#         hypo = predict(mlps, X_cv)
+#         precision = judge()
 #
 # def optimize_num_sample(X_train, y_train, X_cv, y_cv):
 # 	num_sample_opt = 0
@@ -113,10 +114,13 @@ def predict(mlps: '[MLPClassifier]', X: 'nparray') -> 'y_hypo':
     y_hypo = np.argmax(y_hypo_prob, axis = 1).reshape(num_sample, 1)
     return y_hypo
 
-def judge(y: 'nparray', y_hypo: 'nparray') -> 'scalar':
+def judge(**kwargs) -> 'scalar':
     '''\
         true positive / sample number
     '''
+    if 'y' not in kwargs or 'y_hypo' not in kwargs:
+        raise ValueError('arguments missing y or y_hypo')
+    y, y_hypo = kwargs['y'], kwargs['y_hypo']
     num_correct = 0
     num_sample = y.shape[0]
     for i in range(0, num_sample):
